58cdeedc8236ba8617b884d879c574abfe9e142d
unknown <lyejiajun99@gmail.com>
2023-07-10 17:08:29 +0800

    feat: add websocket streaming feature

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -50,15 +52,63 @@ def update_item(item_id: int, item: Item):
     '''
     return {"item_name": item.name, "item_id": item_id}
 
+
+'''
+FastAPI Websocket Streaming Example
+'''
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+# sample endpoint to emulate home page
+@app.get("/homepage")
+async def get():
+    return HTMLResponse(html)
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+
 # additional things to learn
 # cookies, form fields, file, headers
 # other validation constraints like maximum length
 # oauth, deeply nested JSON models
 # websockets, CORS, and more
 
-
 # Terminology
-
 
 # run the program (--reload: make the server restart after code changes. Only use for development)
 # uvicorn <file basename>:<app variable> --reload
